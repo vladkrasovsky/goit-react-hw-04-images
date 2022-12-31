6c2fd0b33e8bdb5115cf27a391fa9790049007d4
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-12-31 09:34:39 +0200

    Updates modal with hooks

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,48 +1,44 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { Overlay, Wrapper, Image } from './Modal.styled';
 
 const ModalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  static propTypes = {
-    url: PropTypes.string.isRequired,
-    onClose: PropTypes.func.isRequired,
-  };
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscKeydown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscKeydown);
-  }
-
-  handleOverlayClick = ({ target, currentTarget }) => {
+const Modal = ({ url, onClose }) => {
+  const handleOverlayClick = ({ target, currentTarget }) => {
     if (target === currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleEscKeydown = ({ code }) => {
-    if (code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+  useEffect(() => {
+    const handleEscKeydown = ({ code }) => {
+      if (code === 'Escape') {
+        onClose();
+      }
+    };
 
-  render() {
-    const { url } = this.props;
+    window.addEventListener('keydown', handleEscKeydown);
 
-    return createPortal(
-      <Overlay onClick={this.handleOverlayClick}>
-        <Wrapper>
-          <Image src={url} alt="" />
-        </Wrapper>
-      </Overlay>,
-      ModalRoot
-    );
-  }
-}
+    return () => {
+      window.removeEventListener('keydown', handleEscKeydown);
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <Overlay onClick={handleOverlayClick}>
+      <Wrapper>
+        <Image src={url} alt="" />
+      </Wrapper>
+    </Overlay>,
+    ModalRoot
+  );
+};
+
+Modal.propTypes = {
+  url: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
 
 export default Modal;
